7ad51097fc54ad37a6bf0468aba04efdc68f782b
ronenfe <ronenfes@gmail.com>
2016-11-18 00:48:49 -0500

    Initial commit

---
+++ app/src/main/java/com/palpair/MainActivity.java
@@ -0,0 +1,90 @@
+package com.palpair;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v13.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setPermissions();
+        setContentView(R.layout.activity_main);
+        WebView webview = (WebView) findViewById(R.id.webview);
+        setUpWebViewDefaults(webview);
+        webview.loadUrl("https://palpair.com");
+        webview.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
+    }
+
+    private void setPermissions() {
+        ArrayList<String> arraylist = new ArrayList<String>();
+        boolean flag;
+        if (ContextCompat.checkSelfPermission(this, "android.permission.RECORD_AUDIO") != PackageManager.PERMISSION_GRANTED)
+            arraylist.add("android.permission.RECORD_AUDIO");
+
+        if (ContextCompat.checkSelfPermission(this, "android.permission.CAMERA") != PackageManager.PERMISSION_GRANTED)
+            arraylist.add("android.permission.CAMERA");
+        if (arraylist.size() > 0) {
+            ActivityCompat.requestPermissions(this, arraylist.toArray(new String[0]), 1);
+        }
+    }
+
+
+    /**
+     * Convenience method to set some generic defaults for a
+     * given WebView
+     *
+     * @param webView
+     */
+    private void setUpWebViewDefaults(WebView webView) {
+        WebSettings settings = webView.getSettings();
+        settings.setAppCacheEnabled(false);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        // Enable Javascript
+        settings.setJavaScriptEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        // Use WideViewport and Zoom out if there is no viewport defined
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+
+
+        // Allow use of Local Storage
+        settings.setDomStorageEnabled(true);
+
+        settings.setDisplayZoomControls(false);
+
+        // Enable remote debugging via chrome://inspect
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // AppRTC requires third party cookies to work
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+    }
+}
